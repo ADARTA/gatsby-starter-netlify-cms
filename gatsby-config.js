9decaf7c522c92b12ee93174f95452e7d1bbd4a9
talves <tony@3alves.com>
2019-04-06 08:56:34 -0700

    use beta plugin for cms
    Until official gatsby plugin gets released.

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: '@talves/gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
         manualInit: true,
