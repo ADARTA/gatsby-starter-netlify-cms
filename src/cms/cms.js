39295d0aac681723b801316eb3f69fcd8f2a6902
talves <tony@3alves.com>
2019-04-06 08:58:39 -0700

    using netlify-cms-app

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,12 @@
-import CMS, { init } from 'netlify-cms'
+import CMS from 'netlify-cms-app'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import FileSystemBackend from 'netlify-cms-backend-fs'
 
-CMS.init = init
 const config = { }
 if (process.env.NODE_ENV === 'development') {
-  const { FileSystemBackend } = require('netlify-cms-backend-fs');
   // config.load_config_file = false
   config.backend = {
     "name": "file-system",
@@ -17,7 +16,8 @@ if (process.env.NODE_ENV === 'development') {
   CMS.registerBackend('file-system', FileSystemBackend)
 } else {
   config.backend = {
-    "name": "git-gateway",
+    "name": "github",
+    "repo": "ADARTA/gatsby-starter-netlify-cms",
     "branch": "master"
   }
 }
